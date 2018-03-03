c76f741dd176288777115cd0d1bce236694a38dd
Scott Serage <xttocsurge51@gmail.com>
2018-03-02 23:15:00 -0700

    Fixed up App component layout, adjusted css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import themes from './assets/constants/themes';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 /* Components */
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Main from './components/Main/Main';
 
-function App(){
+export default function App(){
   return (
-    <Main/>
+    <MuiThemeProvider muiTheme={getMuiTheme(themes.theme1, themes.zIndex)}>
+      <Main/>
+    </MuiThemeProvider>
   );
-}
-
-export default App;
+};
