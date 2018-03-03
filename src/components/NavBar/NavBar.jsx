df6a319734c9ea001c370dc66499cf9c2f7ef4e3
Scott Serage <xttocsurge51@gmail.com>
2018-03-02 23:18:48 -0700

    Added AppBar Material-ui component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,31 +2,41 @@ import React from 'react';
 import './NavBar.css';
 /* Components */
 import {Link} from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import Menu from 'material-ui/svg-icons/navigation/menu';
 
 function NavBar() {
   return (
-    <nav>
-      <Link to=''>
-        Home
-      </Link>
+    <AppBar
+      title="Home"
+      showMenuIconButton={false}
+      iconElementRight={<IconButton><Menu/></IconButton>}
+    />
 
-      <Link to=''>
-        About Us
-      </Link>
-
-      <Link to=''>
-        Training to Adoption
-      </Link>
-      
-      <Link to=''>
-        Training Troubleshooting
-      </Link>
-
-      <Link to=''>
-        Online Store
-      </Link>
-    </nav>
   );
 }
 
 export default NavBar;
+
+// <nav>
+//   <Link to=''>
+//     Home
+//   </Link>
+//
+//   <Link to=''>
+//     About Us
+//   </Link>
+//
+//   <Link to=''>
+//     Training to Adoption
+//   </Link>
+//
+//   <Link to=''>
+//     Training Troubleshooting
+//   </Link>
+//
+//   <Link to=''>
+//     Online Store
+//   </Link>
+// </nav>
