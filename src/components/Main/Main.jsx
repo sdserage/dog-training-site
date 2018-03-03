fa6235ce9b46b804bc00ad40ebefc2fed9555cdb
Scott Serage <xttocsurge51@gmail.com>
2018-03-02 19:41:23 -0700

    Added routing links to Nav, updated the Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+/* Components */
+import router from '../../router';
+import NavBar from '../NavBar/NavBar';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function Main() {
   return (
-    <div>
-      Test
-    </div>
+    <Router>
+      <div>
+        <NavBar/>
+        {router}
+      </div>
+    </Router>
   );
 }
 
