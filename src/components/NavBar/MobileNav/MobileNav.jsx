c89d470948e5e9d4e9227a44a23e312c0f2ed525
Scott Serage <xttocsurge51@gmail.com>
2018-03-06 18:44:13 -0700

    Further tweaked NavMenu - Moved Popover info into an new component

--- src/components/NavBar/MobileNav/MobileNav.jsx
+++ src/components/NavBar/MobileNav/MobileNav.jsx
@@ -2,24 +2,17 @@ import React, {Component} from 'react';
 import '../NavBar.css';
 /* Components */
 import {Link} from 'react-router-dom';
+import LogoIconSVG from './functionalComponents/LogoIconSVG';
+import NavMenu from './functionalComponents/NavMenu';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
-import SvgIcon from 'material-ui/SvgIcon';
 import MenuIcon from 'material-ui/svg-icons/navigation/menu';
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
-import Popover from 'material-ui/Popover';
-import LogoIconSVG from './LogoIconSVG';
 
 export default class MobileNav extends Component {
   constructor(props){
     super(props);
     this.state = {
-      open: false,
-      iconStyles: {
-        width: '200px',
-        height: '50px'
-      }
+      open: false
     };
   };
 
@@ -39,7 +32,7 @@ export default class MobileNav extends Component {
 
   render(){
     const {handleClick, handleRequestClose} = this;
-    const {open, anchorEl, iconStyles} = this.state;
+    const {open, anchorEl} = this.state;
     return (
       <nav className="mobile-navbar">
         <AppBar
@@ -60,40 +53,11 @@ export default class MobileNav extends Component {
               <MenuIcon/>
             </IconButton>
         }/>
-        <Popover
-          className="mobile-navbar-popover"
+        <NavMenu
           open={open}
           anchorEl={anchorEl}
-          anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          onRequestClose={handleRequestClose}
-        >
-          <Menu>
-            <Link to='/' onClick={handleRequestClose}>
-              <MenuItem primaryText='Home'/>
-            </Link>
-
-            <Link to='/about' onClick={handleRequestClose}>
-              <MenuItem primaryText='About Us'/>
-            </Link>
-
-            <Link to='/services' onClick={handleRequestClose}>
-              <MenuItem primaryText='Our Services'/>
-            </Link>
-
-            <Link to='/adoption' onClick={handleRequestClose}>
-              <MenuItem primaryText='Training to Adoption'/>
-            </Link>
-
-            <Link to='/troubleshooting' onClick={handleRequestClose}>
-              <MenuItem primaryText='Training Troubleshooting'/>
-            </Link>
-
-            <Link to='/store' onClick={handleRequestClose}>
-              <MenuItem primaryText='Online Store'/>
-            </Link>
-          </Menu>
-        </Popover>
+          handleRequestClose={handleRequestClose}
+        />
       </nav>
     );
   };
