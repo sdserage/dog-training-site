f40ba13be3df089bb8d6bf7460add34cb5e222af
Scott Serage <xttocsurge51@gmail.com>
2018-03-07 23:19:18 -0700

    Further staged the Home, test parallax feature

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
+import './Home.css';
 /* Images */
+import whiteLogo from '../../assets/images/nlolWhite.svg';
+import otherLogo from '../../assets/images/nlolCustom.svg';
 import pensiveDog from '../../assets/images/pensiveDog.JPG';
 import dogWithBall from '../../assets/images/dogWithBall.JPG';
 import playfulDog from '../../assets/images/playfulDog.JPG';
+import runningPuppies from '../../assets/images/runningPuppies.jpg';
 /* Components */
 import {Parallax, Background} from 'react-parallax';
 
 function Home() {
   return (
-    <section>
+    <section id='home'>
       <Parallax
         blur={0}
-        bgImage={pensiveDog}
+        bgImage={runningPuppies}
         bgImageAlt="dog"
-        strength={200}
+        bgClassName="home-parallax-img"
+        strength={100}
       >
-      Home Content
-        <div style={{ height: '400px' }} />
+        <div
+          className='home-parallax'
+        >
+          <img
+            className='home-logo'
+            src={whiteLogo}
+            alt='New Lead on Life Dog Training'
+          />
+        </div>
       </Parallax>
       <Parallax
-      blur={{ min: -15, max: 15 }}
-      bgImage={dogWithBall}
-      bgImageAlt="the dog with ball"
-      strength={200}
-    >
-      Blur transition from min to max
-      <div style={{ height: '400px' }} />
-    </Parallax>
+        blur={{ min: 15, max: -15 }}
+        bgImage={dogWithBall}
+        bgImageAlt="the dog with ball"
+        strength={200}
+      >
+        Blur transition from min to max
+        <div style={{ height: '1000px', width: '100vw' }} />
+      </Parallax>
     <Parallax strength={300} bgImage={playfulDog}>
       <div style={{ height: '400px' }}>Use the background component for custom elements</div>
 
