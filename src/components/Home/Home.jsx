e6cd369c849732561b2cc0f7c2354ec474e76a61
Scott Serage <xttocsurge51@gmail.com>
2018-03-08 18:58:11 -0700

    Removed Parallax component for now

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import './Home.css';
 /* Images */
-import whiteLogo from '../../assets/images/nlolWhite.svg';
 import blackLogo from '../../assets/images/nlolBlack.png';
-import otherLogo from '../../assets/images/nlolCustom.svg';
-import pensiveDog from '../../assets/images/pensiveDog.JPG';
-import dogWithBall from '../../assets/images/dogWithBall.JPG';
-import playfulDog from '../../assets/images/playfulDog.JPG';
-import runningPuppies from '../../assets/images/runningPuppies.jpg';
-/* Components */
-import {Parallax, Background} from 'react-parallax';
 
 function Home() {
   return (
@@ -26,8 +18,3 @@ function Home() {
 }
 
 export default Home;
-
-
-// <Background className="custom-bg">
-//   <img src="../../assets/images/playfulDog.JPG" alt="playful dog" />
-// </Background>
