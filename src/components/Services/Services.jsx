31ea5ddbeb7041905580b6aed52fe8cfd44fe6f4
Scott Serage <xttocsurge51@gmail.com>
2018-03-12 22:24:16 -0600

    Started basics for additional routes

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Services.css';
+/* Images */
+
+/* Components */
+
+function Services() {
+  return (
+    <section id='services'>
+
+    </section>
+  );
+}
+
+export default Services;
--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-// Components
+/* Components */
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Services from './components/Services/Services';
+import Adoption from './components/Adoption/Adoption';
+import Troubleshooting from './components/Troubleshooting/Troubleshooting';
+import Store from './components/Store/Store';
+// import NotFound from './components/NotFound/NotFound';
 
 export default (
   <Switch>
-    <Route exact path='/' component={Home}/>
-    <Route path='/about' component={About}/>
-    <Route path='/*' component={Home}/>
+    <Route exact path='/'           component={Home}/>
+    <Route path='/about'            component={About}/>
+    <Route path='/services'         component={Services}/>
+    <Route path='/adoption'         component={Adoption}/>
+    <Route path='/troubleshooting'  component={Troubleshooting}/>
+    <Route path='/store'            component={Store}/>
+    {/*<Route path='/*'                component={NotFound}/>*/}
+    <Route path='/*'                component={Home}/>
   </Switch>
 );
