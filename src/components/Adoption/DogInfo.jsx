a7d902aaaa3a4ab994a32c9924dfc3847b48270f
Scott Serage <xttocsurge51@gmail.com>
2018-03-24 16:16:03 -0600

    Implemented the Paragraph component and changed styling to match it

--- src/components/Adoption/DogInfo.jsx
+++ src/components/Adoption/DogInfo.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+/* Components */
+import Paragraph from '../../assets/functionalComponents/Paragraph/Paragraph';
+
 export default function DogInfo(props) {
-  const {image, name, description, summary, /*linkInfo,*/ index} = props;
+  const {image, name, description, summary, summaryLinks, index} = props;
   return (
     <li className={`dog-info ${index === 0 ? 'dog-info-1st' : ''}`}>
       <h3>{name}</h3>
       <img src={image} alt={name}/>
-      <p>{description}</p>
-      <h5>{summary}</h5>
+      <Paragraph className='description' textBlock={description}/>
+      <Paragraph className='summary' textBlock={summary} linksArr={summaryLinks}/>
     </li>
   );
 }
