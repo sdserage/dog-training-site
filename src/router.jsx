1e3dc0466c432bbb18a1e2391c28eaf9fabc157a
Scott Serage <xttocsurge51@gmail.com>
2018-03-26 17:56:17 -0600

    Found a useful component for making sure new pages start at the top

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 /* Components */
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Services from './components/Services/Services';
@@ -10,13 +11,15 @@ import Store from './components/Store/Store';
 import NotFound from './components/NotFound/NotFound';
 
 export default (
-  <Switch>
-    <Route exact path='/'           component={Home}/>
-    <Route path='/about'            component={About}/>
-    <Route path='/services'         component={Services}/>
-    <Route path='/adoption'         component={Adoption}/>
-    <Route path='/troubleshooting'  component={Troubleshooting}/>
-    <Route path='/store'            component={Store}/>
-    <Route path='*'                 component={NotFound}/>
-  </Switch>
+  <ScrollToTop>
+    <Switch>
+      <Route exact path='/'           component={Home}/>
+      <Route path='/about'            component={About}/>
+      <Route path='/services'         component={Services}/>
+      <Route path='/adoption'         component={Adoption}/>
+      <Route path='/troubleshooting'  component={Troubleshooting}/>
+      <Route path='/store'            component={Store}/>
+      <Route path='*'                 component={NotFound}/>
+    </Switch>
+  </ScrollToTop>
 );
